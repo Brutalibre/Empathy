7076bffd6944f2031b83c2dcedaa0a47b492878b
Val Isautier <val.isautier@gmail.com>
2018-02-11 00:48:55 +0100

    Prepared assets for Healing feature coding.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -11,6 +11,9 @@ public class GameController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Cursor.visible)
+        {
+            Cursor.visible = false;
+        }
 	}
 }
