622dbbcb02983000d4d354b0cc69eca94b958452
Val Isautier <val.isautier@gmail.com>
2018-02-28 13:26:55 +0100

    Added circle interactions

---
+++ Assets/Prefabs/Animal/Dog.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dog : MonoBehaviour {
+
+
+    public Animator DogAnimator;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ChangeAnimationToMad()
+    {
+        DogAnimator.SetBool("Close_Circle", true);
+    }
+
+    public void ChangeAnimationToIdle()
+    {
+        DogAnimator.SetBool("Close_Circle", false);
+    }
+}
--- Assets/Scripts/TalkableScript.cs
+++ Assets/Scripts/TalkableScript.cs
@@ -9,10 +9,14 @@ public class TalkableScript : MonoBehaviour {
     public Color HighlightColor = new Color(1, 0.929f, 0.0f);
 
     protected bool isColliding = false;
+    protected bool canCollide = false;
    
 	// Use this for initialization
 	protected void Start () {
         rend = GetComponent<SpriteRenderer>();
+        if (!rend)
+            rend = GetComponentInChildren<SpriteRenderer>();
+
         baseCol = rend.color;
     }
 	
@@ -23,9 +27,10 @@ public class TalkableScript : MonoBehaviour {
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Cursor") {
-            rend.color = HighlightColor;
-            isColliding = true;
+        if (collision.tag == "Cursor")
+        {
+            ChangeColor();
+            Collide(true);
         }
     }
 
@@ -33,8 +38,37 @@ public class TalkableScript : MonoBehaviour {
     {
         if (collision.tag == "Cursor")
         {
-            rend.color = baseCol;
-            isColliding = false;
+            ChangeColor();
+            Collide(false);
         }
+    }
+
+    public void Collide (bool isCol)
+    {
+        if (canCollide)
+            isColliding = isCol;
+
+        else
+            isColliding = false;
+    }
+
+    public void ChangeColor()
+    {
+        if (canCollide)
+            rend.color = (rend.color == baseCol) ? HighlightColor : baseCol;
+
+        else
+            rend.color = baseCol;
+    }
+
+    // These functions are invoked when the player steps in or out an Outer Circle.
+    public void CanCollide()
+    {
+        canCollide = true;
+    }
+
+    public void CannotCollide()
+    {
+        canCollide = false;
     }
 }
